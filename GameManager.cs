190e60225d3f8dcaf2111af1ba5fae5cff8c8d42
Martijn <martijn.dormans@gmail.com>
2016-09-24 14:13:42 +0200

    Some of the files from my misc file

---
+++ GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    private static Dictionary<string, Player> Players = new Dictionary<string, Player>();
+
+    public static void RegisterPlayer(string _NetID, Player _Player)
+    {
+        string PlayerID = "Player " + _NetID;
+        Players.Add(PlayerID, _Player);
+        _Player.transform.name = PlayerID;
+    }
+    public static void DeRegisterPlayer(string _PlayerID) //wil je de functie roepen gebruik dan transform.name = _PlayerID
+    {
+        Players.Remove(_PlayerID);
+    }
+    public static Player GetPlayer(string _PlayerID)
+    {
+        return Players[_PlayerID];
+    }
+}
